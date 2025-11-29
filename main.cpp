68c059f4e23454ae4b3072b353da9d7c2f45219c
gui castaldeli <guicastaldeli8@gmail.com>
2025-11-29 16:29:33 -0400

    update-01_03

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("HWELLOW 1guiROLD!!!asadadddasddasd");
-    return 0;
-}
---
+++ root/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("C PLUS PLUS!");
+    return 0;
+}

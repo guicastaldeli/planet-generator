06e5b44936abffdb6cdbc8f06a5976f9953ffcc4
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-07 23:01:21 -0400

    update-05_05

--- root/.buffers/buffers.cpp
+++ root/.buffers/buffers.cpp
@@ -57,19 +57,15 @@ void Buffers::set(BufferData::Type type) {
         GL_STATIC_DRAW
     );
 
-    // Position attribute (3 floats)
     GLuint posAttr = glGetAttribLocation(shaderController->shaderProgram, "aPos");
     if(posAttr != -1) {
-        glVertexAttribPointer(posAttr, 3, GL_FLOAT, GL_FALSE, 
-                              5 * sizeof(float), (void*)0);
+        glVertexAttribPointer(posAttr, 3, GL_FLOAT, GL_FALSE,  5 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(posAttr);
     }
 
-    // Texture coordinate attribute (2 floats, starts after 3 position floats)
     GLuint texCoordAttr = glGetAttribLocation(shaderController->shaderProgram, "aTexCoord");
     if(texCoordAttr != -1) {
-        glVertexAttribPointer(texCoordAttr, 2, GL_FLOAT, GL_FALSE, 
-                              5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glVertexAttribPointer(texCoordAttr, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(texCoordAttr);
     }
 
